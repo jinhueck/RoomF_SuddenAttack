d86ff2b2c3b181e33b4f70971f248cfedfa41ed8
burgii <qorgh9184@gmail.com>
2019-01-15 16:36:56 +0900

    Photon 기본 연결 Resources 폴더 추가

--- Assets/Script/JinHyeok/PhotonNetworkManager.cs
+++ Assets/Script/JinHyeok/PhotonNetworkManager.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PhotonNetworkManager : MonoBehaviour {
+public class PhotonNetworkManager : Photon.PunBehaviour {
 
+    [SerializeField] private Text connectText;
 	// Use this for initialization
 	void Start () {
-		
+        PhotonNetwork.ConnectUsingSettings("0.1");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        connectText.text = PhotonNetwork.connectionStateDetailed.ToString();
+
+    }
 }
